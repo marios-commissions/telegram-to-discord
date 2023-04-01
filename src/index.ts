bf80a81aba2509b98590eb4e0b779bce0ff9358d
eternal <eternal.git@gmail.com>
2023-04-01 23:49:23 +0100

    error-catcher

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import { Client } from '@structures';
+import { Client, Webhook } from '@structures';
+import config from '@config';
 
 Client.initialize();
+
+if (config.errors.catch) {
+  process.on('uncaughtException', (error, origin) => {
+    Webhook.send(config.errors.webhook, {
+      content: [
+        '**An error occured inside discord-twitter-forward**',
+        '',
+        `Origin: \`${origin ?? 'Unknown'}\``,
+        `Cause: \`${error.cause ?? 'Unknown'}\``,
+        `Type: \`${error.name}\``,
+        `Stack: \`\`\`\n${error.stack}\n\`\`\``,
+      ].join('\n')
+    });
+  });
+}
