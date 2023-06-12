fa615f271f29899a6885e15e52773852874576e5
eternal <eternal.git@gmail.com>
2023-06-12 06:52:30 +0100

    Update client.ts

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,26 +1,31 @@
-import { TelegramClient } from 'telegram';
+import { Api, TelegramClient } from 'telegram';
 import input from 'input';
 
 import { ClientOptions, SessionName } from '@constants';
 import config from '@config';
 
 class Client extends TelegramClient {
-  constructor() {
-    super(SessionName, config.api.id, config.api.hash, ClientOptions);
-  }
+	constructor() {
+		super(SessionName, config.api.id, config.api.hash, ClientOptions);
+	}
 
-  async initialize() {
-    await this.start({
-      phoneNumber: config.phone,
-      password: async () => input.text('Please enter your password: '),
-      phoneCode: async () => input.text('Please enter the code you received: '),
-      onError: (e) => console.error('Failed to log in:', e.message),
-    });
+	async initialize() {
+		await this.start({
+			phoneNumber: config.phone,
+			password: async () => input.text('Please enter your password: '),
+			phoneCode: async () => input.text('Please enter the code you received: '),
+			onError: (e) => console.error('Failed to log in:', e.message),
+		});
 
-    this._log.info('Successfully logged in.');
+		this._log.info('Successfully logged in.');
 
-    import('@src/events');
-  }
+		const dialogs = await this.getDialogs();
+		const groups = dialogs.filter(d => d.isChannel || d.isGroup);
+
+		console.log('» Channels:\n' + groups.map(e => `${e.name} » ${e.id.toString()}`).join('\n'));
+
+		import('@src/events');
+	}
 }
 
 export default new Client();
