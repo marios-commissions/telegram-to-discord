92684ffc76367bed28fa482f786000b49c595f5c
Mario <me@marioparaschiv.com>
2024-07-20 03:57:55 +0100

    ✨ Improve start-up banner

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,5 @@
 import { ClientOptions, SessionName } from '~/constants';
-import { TelegramClient } from 'telegram';
+import { Api, TelegramClient } from 'telegram';
 import config from '~/config';
 import input from 'input';
 
@@ -21,7 +21,46 @@ class Client extends TelegramClient {
 		const dialogs = await this.getDialogs();
 		const groups = dialogs.filter(d => d.isChannel || d.isGroup);
 
-		console.log('» Channels:\n' + groups.map(e => `${e.name} » ${e.id.toString()}`).join('\n'));
+
+		const banner = [];
+
+
+		const promises = [];
+
+		for (const group of groups) {
+			promises.push(new Promise(async (resolve) => {
+				const entity = await this.getEntity(group.id);
+
+				const isDM = entity.className === 'User';
+				const isForum = (entity as Api.Channel).forum;
+				const isLinked = (entity as Api.Channel).hasLink || (entity as Api.Channel).broadcast;
+
+				const topics = [];
+
+				if (isForum) {
+					const request = await this.invoke(new Api.channels.GetForumTopics({ channel: entity }));
+
+					for (const topic of (request?.topics ?? []) as Api.ForumTopic[]) {
+						topics.push(topic.title);
+					}
+				}
+
+				banner.push([
+					`${group.name} (${group.id.toString()})`,
+					`- Is Pinned: ${group.pinned}`,
+					`- Type: ${isForum ? 'Forum' : isLinked ? 'Linked' : isDM ? 'DM' : 'Group'}`,
+					topics.length && '- Topics',
+					...((topics.length && topics.map(t => `-- ${t}`)) || []),
+					' '
+				].filter(Boolean).join('\n'));
+
+				resolve(true);
+			}));
+		}
+
+		await Promise.all(promises);
+
+		Logger.info('Channels\n' + banner.join('\n'));
 
 		import('~/src/events');
 	}
